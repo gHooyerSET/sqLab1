f0db6d5f41ec58dcc5dd2d355cc570964bde75e6
gHooyerSET <ghooyer6402@conestogac.on.ca>
2021-09-17 11:51:19 -0400

    Added Unit Test: 'rightAreaTest' - Status: FAILS

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -13,10 +13,18 @@ namespace UnitTest
 		
 		TEST_METHOD(hypotenuseTest)
 		{
-			Triangle obj(4.0, 4.0);
+			Triangle obj(4.0, 4.0, true);
 			double hypotenuse = obj.CalculateHypotenuse();
 			double expectedResult = sqrt( ( 4.0 * 4.0 ) + ( 4.0 * 4.0 ) );
 			Assert::AreEqual(hypotenuse, expectedResult);
+		}
+
+		TEST_METHOD(rightAreaTest)
+		{
+			Triangle obj(4.0, 4.0, true);
+			double area = obj.CalculateAreaRightTriangle();
+			double expectedResult = (4.0 * 4.0) * 0.5;
+			Assert::AreEqual(area, expectedResult);
 		}
 	};
 }
--- sqTDDLab/Triangle.h
+++ sqTDDLab/Triangle.h
@@ -7,15 +7,30 @@ private:
 	double sideA, sideB, sideC;
 	double angleA, angleB, angleC;
 public:
-	Triangle::Triangle(double sideA, double sideB)//Incomplete constructor for hypotenuse test
+	Triangle::Triangle(double a, double b, bool areSideLengths)//Incomplete constructor for hypotenuse test
 	{
-		this->sideA = sideA;
-		this->sideB = sideB;
+		if (areSideLengths == true)
+		{
+			this->sideA = a;
+			this->sideB = b;
+			this->sideC = sqrt( (a * a) + (b * b) );
+		}
+		else
+		{
+			this->angleA = a;
+			this->angleB = b;
+			this->angleC = 180 - (a + b);
+		}
 	}
 
 	double Triangle::CalculateHypotenuse()
 	{
-		double retValue = sqrt((sideA * sideA) + (sideB * sideB));
+		double retValue = sqrt( (sideA * sideA) + (sideB * sideB) );
 		return retValue;
+	}
+
+	double Triangle::CalculateAreaRightTriangle()
+	{
+		return 0;
 	}
 }; 
